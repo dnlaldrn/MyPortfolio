064e04809b45bfdb0da09d15b24f17b89bbbe175
Daniel Manjares <danielaldreenm@gmail.com>
2026-01-02 02:37:17 +0800

    Add the components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header-component/header-component';
+import {AboutComponent} from './about-component/about-component';
+import {ExperinceComponent} from './experince-component/experince-component';
+import {TechStackComponent} from './tech-stack-component/tech-stack-component';
+import {CertificationsComponent} from './certifications-component/certifications-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent,ExperinceComponent, TechStackComponent, CertificationsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
