831c9ed75e9969d780e5d1d128460a1680e4c3ae
Daniel Manjares <danielaldreenm@gmail.com>
2026-01-04 17:42:45 +0800

    Add ui for tech-stack component

--- src/app/Components/tech-stack-component/tech-stack-component.ts
+++ src/app/Components/tech-stack-component/tech-stack-component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+interface FrontendTechStackItem {
+  name:string;
+}
+interface BackendTechStackItem {
+  name:string;
+}
+interface OtherTechStackItem {
+  name:string;
+
+}
+
 
 @Component({
   selector: 'app-tech-stack-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tech-stack-component.html',
   styleUrl: './tech-stack-component.css',
 })
 export class TechStackComponent {
-
+  techStackItems: FrontendTechStackItem[] = [
+    { name: 'HTML' },
+    { name: 'CSS' },
+    { name: 'JavaScript' },
+    { name: 'TypeScript' },
+    { name: 'Angular' },
+    { name: 'Tailwind CSS' },
+  ];
+  backendTechStackItems: BackendTechStackItem[] = [
+    {name: 'Node.js' },
+    {name: 'Express.js' },
+    {name: 'MySql'}
+  ]
+  otherTechStackItems: OtherTechStackItem[] = [
+    {name: 'Git' },
+    {name: 'GitHub' },
+    {name: 'Prettier' },
+    {name: 'Vercel' }
+  ]
 }
