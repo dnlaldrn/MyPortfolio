3bbe3e0489e30b039c02ce31924498cf04ec4ee4
Daniel Manjares <danielaldreenm@gmail.com>
2026-01-07 05:49:16 +0800

    Add layout for the components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header-component/header-component';
-import {AboutComponent} from './about-component/about-component';
-import {ExperinceComponent} from './experince-component/experince-component';
-import {TechStackComponent} from './tech-stack-component/tech-stack-component';
-import {CertificationsComponent} from './certifications-component/certifications-component';
+import { HeaderComponent } from './Components/header-component/header-component';
+import { AboutComponent } from './Components/about-component/about-component';
+import { ExperinceComponent } from './Components/experince-component/experince-component';
+import { TechStackComponent } from './Components/tech-stack-component/tech-stack-component';
+import { CertificationsComponent } from './Components/certifications-component/certifications-component';
+import { ProjectsComponent } from './Components/projects-component/projects-component';
+import { SocialLinksComponent } from './Components/social-links-component/social-links-component';
+import { OrganizationComponent } from './Components/organization-component/organization-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AboutComponent,ExperinceComponent, TechStackComponent, CertificationsComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AboutComponent,
+    ExperinceComponent,
+    TechStackComponent,
+    CertificationsComponent,
+    ProjectsComponent,
+    SocialLinksComponent,
+    OrganizationComponent,
+    
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('MyPortfolio');
