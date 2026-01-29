acab4f8b2f17f4099c00aeb86483791336b81ce2
Daniel Manjares <danielaldreenm@gmail.com>
2026-01-29 08:28:13 +0800

    Add page folder

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../Components/header-component/header-component'
+import { AboutComponent } from '../../Components/about-component/about-component';
+import { ExperinceComponent } from '../../Components/experince-component/experince-component';
+import { TechStackComponent } from '../../Components/tech-stack-component/tech-stack-component';
+import { CertificationsComponent } from '../../Components/certifications-component/certifications-component';
+import { ProjectsComponent } from '../../Components/projects-component/projects-component';
+import { SocialLinksComponent } from '../../Components/social-links-component/social-links-component';
+import { OrganizationComponent } from '../../Components/organization-component/organization-component';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [
+      RouterOutlet,
+      HeaderComponent,
+        AboutComponent,
+        ExperinceComponent,
+        TechStackComponent,
+        CertificationsComponent,
+        ProjectsComponent,
+        SocialLinksComponent,
+        OrganizationComponent,
+  ],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css',
+})
+export class HomePage {
+
+}
