7bdea3b6217b5dccc58b75d941abd9904025853c
Daniel Manjares <danielaldreenm@gmail.com>
2026-01-31 16:17:38 +0800

    Add darkmode logic

--- src/app/services/theme.ts
+++ src/app/services/theme.ts
@@ -4,5 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Theme {
+  private dark = false;
+
+  constructor(){
+    const saved = localStorage.getItem('theme');
+    this.dark = saved === 'dark';
+    this.applyTheme();
+  }
+
+  toggleTheme(){
+    this.dark = !this.dark;
+    localStorage.setItem('theme', this.dark? 'dark': 'light');
+    this.applyTheme();
+    
+  }
+  private applyTheme(){
+    document.body.classList.toggle('dark', this.dark);
+  }
+  isDark(){
+    return this.dark;
+  }
   
 }
