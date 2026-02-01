ce3194b7d552886ef4796b20387ac21f05dbe588
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-02 04:58:17 +0800

    Add gallery component ui

--- src/app/Components/gallery-component/gallery-component.ts
+++ src/app/Components/gallery-component/gallery-component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {  Component, inject } from '@angular/core';
+import {GalleryService, slideElements} from '../../services/gallery/gallery';
+
+
+
 
 @Component({
   selector: 'app-gallery-component',
+  standalone:true,
   imports: [],
   templateUrl: './gallery-component.html',
   styleUrl: './gallery-component.css',
 })
-export class GalleryComponent {
+export class GalleryComponent  {
+  slides : slideElements[] =[]
+
+  private galleryserice = inject(GalleryService)
+
+  ngOnInit():void{
+    this.slides = this.galleryserice.getAllElements();
+  }
+
+  
+
+  
+
 
 }
