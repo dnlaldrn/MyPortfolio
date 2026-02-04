67643748192a9ff69467b18a21490fdcb540b30e
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-04 19:05:21 +0800

    Add journey ui

---
+++ src/app/pages/journey-component/journey-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {GalleryPage} from '../../Components/gallery-page/gallery-page';
+
+@Component({
+  selector: 'app-journey-component',
+  imports: [GalleryPage],
+  templateUrl: './journey-component.html',
+  styleUrl: './journey-component.css',
+})
+export class JourneyComponent {
+
+}
