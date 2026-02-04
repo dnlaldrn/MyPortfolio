236f1eecc5e25766de9125bb23e94b7f3f848ed1
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-04 22:58:47 +0800

    Add animation

--- src/app/Components/gallery-page/gallery-page.ts
+++ src/app/Components/gallery-page/gallery-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component , inject} from '@angular/core';
+import {slideElements, GalleryService } from '../../services/gallery/gallery';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-gallery-page',
-  imports: [],
+  standalone:true,
+  imports: [RouterLink],
   templateUrl: './gallery-page.html',
   styleUrl: './gallery-page.css',
 })
 export class GalleryPage {
+
+  slides : slideElements[]=[];
+
+  private galleryservice = inject(GalleryService);
+
+  ngOnInit():void{
+    this.slides = this.galleryservice.getAllElements();
+  }
 
 }
