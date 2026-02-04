b014639d929d7a9a0793e8df3b178ef0dcf18e43
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-04 19:08:18 +0800

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: "",
+    loadComponent: () => import("./pages/home-page/home-page").then((x) => x.HomePage),
+  },
+  {
+    path: "journey",
+    loadComponent: () => import("./pages/journey-component/journey-component").then((x) => x.JourneyComponent),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import {HomePage} from './pages/home-page/home-page'
+import {HomePage} from './pages/home-page/home-page';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomePage],
+  imports: [HomePage, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
