782b36b5728c487c613b7617e2f7a0e536823838
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-06 18:05:11 +0800

    Update the services of the gallerty

--- src/app/services/gallery/gallery.ts
+++ src/app/services/gallery/gallery.ts
@@ -22,19 +22,43 @@ export class GalleryService {
       link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
     },
      {
-      img : '/images/Gallery/Day3.png',
+      img : '/images/Gallery/Day4.png',
       link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
     },
     {
-      img : '/images/Gallery/Day3.png',
+      img : '/images/Gallery/Day5.png',
       link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
     },
     {
-      img : '/images/Gallery/Day3.png',
+      img : '/images/Gallery/Day6.png',
       link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
     },
     {
-      img : '/images/Gallery/Day3.png',
+      img : '/images/Gallery/Day7.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day8.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day9.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day10.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day11.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day12.png',
+      link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
+    },
+    {
+      img : '/images/Gallery/Day13.png',
       link : 'https://www.tiktok.com/@danielmanjares_on_ig/video/7589535539137694997'
     },
     
