0abb0df315ae94c9964cf2ff569adf9c6fa98f3c
Daniel Manjares <danielaldreenm@gmail.com>
2026-02-12 11:04:54 +0800

    Remove unessesacy import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import {HomePage} from './pages/home-page/home-page';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomePage, RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
