49cb114823afc239c363d2172dc5758fac00af86
Daniel Manjares <danielaldreenm@gmail.com>
2026-03-23 18:09:37 +0800

    Add ai model

--- src/app/services/gemini/gemini.ts
+++ src/app/services/gemini/gemini.ts
@@ -5,10 +5,20 @@ import { environment } from '../../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class GeminiService {
   private genAI = new GoogleGenerativeAI(environment.apiKey);
-  private model = this.genAI.getGenerativeModel({ model: 'gemini-3-flash-preview' }); 
 
   async generateResponse(prompt: string) {
-    const result = await this.model.generateContent(prompt);
+    const model = this.genAI.getGenerativeModel({ 
+      model: 'gemini-3-flash-preview',
+      systemInstruction: "You are Daniel Aldreen Manjares living in Batangas City.You are good at angular and tailwind for front-end and you are good at using github, git prettier vercel and figma. You are looking forward on learning back-end . You are Daniel's assistant. ALWAYS answer in PLAIN TEXT only Make it short and simple. Do not use Markdown, bolding, or asterisks (**) for emphasis. "
+    }); 
+    
+    const result = await model.generateContent({
+      contents: [{ role: 'user', parts: [{ text: prompt }] }],
+      generationConfig: { 
+        temperature: 0.7,
+      }
+    });
+
     const response = await result.response;
     return response.text();
   }
