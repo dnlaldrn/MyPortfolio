2d173032327ce3d6e40a2586bc9c006ddb7564d7
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-04-30 14:42:52 +0800

    Update the projects

--- src/app/Components/projects-component/projects-component.ts
+++ src/app/Components/projects-component/projects-component.ts
@@ -18,10 +18,28 @@ export class ProjectsComponent {
   constructor(public theme: Theme) {}
   projectItems: ProjectItems[] = [
     {
-      name: 'PromdiFarm',
-      description: 'A marketplace for Filipino farmers',
+      name: 'San Juan Senior High School Web',
+      description: 'A prototype web-based school platform designed to explore how students can access academic resources, announcements, and essential information in a centralized system.',
+      status: 'Completed',
+      url: 'sjshs.vercel.app',
+    },
+     {
+      name: 'Environmental Science',
+      description: 'An interactive web platform for environmental science that presents educational content, resources, and insights to promote awareness and understanding of environmental issues and sustainability',
       status: 'On progress',
-      url: 'promdifarm.com',
+      url: 'envsci.vercel.app',
+    },
+    {
+      name: 'Rock Paper And Scissor Game',
+      description: 'A simple Rock–Paper–Scissors game developed to strengthen my programming fundamentals.',
+      status: 'Completed',
+      url: 'https://dnlaldrn.github.io/Rock-Paper-Scissors/',
+    },
+    {
+      name: 'Stanza',
+      description: 'An ongoing web-based dorm listing app for Batangas City',
+      status: 'On progress',
+      url: 'Coming soon',
     },
   ];
 }
