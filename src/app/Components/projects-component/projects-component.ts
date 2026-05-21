ff8c3fa72f2c14635b042af7003a478b5600d25e
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-21 18:41:00 +0800

    Add changes in the project component ui

--- src/app/Components/projects-component/projects-component.ts
+++ src/app/Components/projects-component/projects-component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { Theme } from '../../services/theme';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 interface ProjectItems {
   name: string;
   description: string;
-  status: string;
+  completed: boolean;
   url: string;
 }
 
 @Component({
   selector: 'app-projects-component',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './projects-component.html',
   styleUrl: './projects-component.css',
 })
@@ -20,26 +22,27 @@ export class ProjectsComponent {
     {
       name: 'San Juan Senior High School Web',
       description: 'A prototype web-based school platform designed to explore how students can access academic resources, announcements, and essential information in a centralized system.',
-      status: 'Completed',
+      completed: true,
       url: 'sjshs.vercel.app',
     },
      {
       name: 'Environmental Science',
       description: 'An interactive web platform for environmental science that presents educational content, resources, and insights to promote awareness and understanding of environmental issues and sustainability',
-      status: 'On progress',
+      completed: true,
       url: 'envsci.vercel.app',
     },
     {
       name: 'Rock Paper And Scissor Game',
       description: 'A simple Rock–Paper–Scissors game developed to strengthen my programming fundamentals.',
-      status: 'Completed',
+      completed: true,
       url: 'https://dnlaldrn.github.io/Rock-Paper-Scissors/',
     },
     {
       name: 'Stanza',
       description: 'An ongoing web-based dorm listing app for Batangas City',
-      status: 'On progress',
+      completed: false,
       url: 'Coming soon',
     },
   ];
+
 }
